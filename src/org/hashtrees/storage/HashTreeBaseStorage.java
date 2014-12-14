c454a9a11b61c7736f50fbef17c0ad2a6106c17f
Gomathivinayagam Muthuvinayagam <gomes@yahoo-inc.com>
2014-12-14 08:07:35 -0800

    Adding base storage class

---
+++ src/org/hashtrees/storage/HashTreeBaseStorage.java
@@ -0,0 +1,39 @@
+package org.hashtrees.storage;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.hashtrees.util.AtomicBitSet;
+
+public abstract class HashTreeBaseStorage implements HashTreeStorage {
+
+	private final ConcurrentMap<Long, AtomicBitSet> treeIdAndDirtySegmentMap = new ConcurrentHashMap<Long, AtomicBitSet>();
+	private final int noOfSegDataBlocks;
+
+	public HashTreeBaseStorage(int noOfSegDataBlocks) {
+		this.noOfSegDataBlocks = noOfSegDataBlocks;
+	}
+
+	private AtomicBitSet getDirtySegmentsHolder(long treeId) {
+		if (!treeIdAndDirtySegmentMap.containsKey(treeId))
+			treeIdAndDirtySegmentMap.putIfAbsent(treeId, new AtomicBitSet(
+					noOfSegDataBlocks));
+		return treeIdAndDirtySegmentMap.get(treeId);
+	}
+
+	@Override
+	public void setDirtySegment(long treeId, int segId) {
+		getDirtySegmentsHolder(treeId).set(segId);
+	}
+
+	@Override
+	public List<Integer> clearAndGetDirtySegments(long treeId) {
+		return getDirtySegmentsHolder(treeId).clearAndGetAllSetBits();
+	}
+
+	@Override
+	public void clearAllSegments(long treeId) {
+		getDirtySegmentsHolder(treeId).clear();
+	}
+}
--- src/org/hashtrees/storage/Storage.java
+++ src/org/hashtrees/storage/Storage.java
@@ -3,8 +3,17 @@ package org.hashtrees.storage;
 import java.nio.ByteBuffer;
 import java.util.Iterator;
 
+import org.hashtrees.HashTree;
 import org.hashtrees.util.Pair;
 
+/**
+ * There could be cases where actual storage is missing (key,value) pairs, or
+ * having keys which are not supposed to be there. In these cases, HashTree has
+ * to directly talk to storage interface to do the necessary operations.
+ * 
+ * {@link HashTree} has to be provided with the implementation of this class.
+ * 
+ */
 public interface Storage {
 
 	ByteBuffer get(ByteBuffer key);

1fbfda5c3d8ac34802d382201650ef442a64dca6
Gomathivinayagam Muthuvinayagam <gomes@yahoo-inc.com>
2014-12-15 08:54:11 -0800

    Making atomicbitset class to be growable

--- src/org/hashtrees/util/AtomicBitSet.java
+++ src/org/hashtrees/util/AtomicBitSet.java
@@ -3,14 +3,16 @@ package org.hashtrees.util;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.atomic.AtomicLongArray;
 
 import javax.annotation.concurrent.ThreadSafe;
 
 /**
  * Default {@link BitSet} provided in java is not thread safe. This class
- * provides a minimalistic thread safe version of BitSet also growable.
+ * provides a minimalistic thread safe version of BitSet and also dynamically
+ * allocates any .
  * 
  */
 @ThreadSafe
@@ -21,26 +23,22 @@ public class AtomicBitSet {
 
 	private final static int ADDRESS_BITS_PER_WORD_ATOMIC_ARRAY = 10;
 	private final static int ATOMIC_LONG_ARRAY_SIZE = 1 << ADDRESS_BITS_PER_WORD_ATOMIC_ARRAY;
-	private final ConcurrentHashMap<Integer, AtomicLongArray> bitsHolderMap = new ConcurrentHashMap<>();
+	private final ConcurrentSkipListMap<Integer, AtomicLongArray> bitsHolderMap = new ConcurrentSkipListMap<>();
 
-	private AtomicLongArray getBitsHolderFromMap(int bitIndex) {
-		int wordPos = bitIndex >> ADDRESS_BITS_PER_WORD_ATOMIC_ARRAY;
-		if (!bitsHolderMap.containsKey(wordPos))
-			bitsHolderMap.put(wordPos, new AtomicLongArray(
-					ATOMIC_LONG_ARRAY_SIZE));
-		return bitsHolderMap.get(wordPos);
+	private int getWordPos(int bitIndex) {
+		return bitIndex >> ADDRESS_BITS_PER_WORD_ATOMIC_ARRAY;
 	}
 
-	/**
-	 * Gets the index of the element in {@link #bitsHolder} for the given
-	 * bitIndex.
-	 * 
-	 * @param bitIndex
-	 *            , values can be between 0 and {@link #totBits} - 1
-	 * @return
-	 */
 	private int getWordIndex(int bitIndex) {
-		return bitIndex >> ADDRESS_BITS_PER_WORD;
+		return (bitIndex & (ATOMIC_LONG_ARRAY_SIZE - 1)) >> ADDRESS_BITS_PER_WORD;
+	}
+
+	private AtomicLongArray getBitsHolderFromMap(int bitIndex) {
+		int wordPos = getWordPos(bitIndex);
+		if (!bitsHolderMap.containsKey(wordPos))
+			bitsHolderMap.putIfAbsent(wordPos, new AtomicLongArray(
+					ATOMIC_LONG_ARRAY_SIZE));
+		return bitsHolderMap.get(wordPos);
 	}
 
 	/**
@@ -76,13 +74,15 @@ public class AtomicBitSet {
 
 	public List<Integer> clearAndGetAllSetBits() {
 		List<Integer> result = new ArrayList<Integer>();
-		for (AtomicLongArray bitsHolder : bitsHolderMap.values()) {
+		for (Entry<Integer, AtomicLongArray> bitsHolderEntry : bitsHolderMap
+				.entrySet()) {
+			int offset = bitsHolderEntry.getKey() * ATOMIC_LONG_ARRAY_SIZE;
+			AtomicLongArray bitsHolder = bitsHolderEntry.getValue();
 			for (int i = 0; i < bitsHolder.length(); i++) {
 				long oldValue = bitsHolder.get(i);
 				while (!bitsHolder.compareAndSet(i, oldValue, 0))
 					oldValue = bitsHolder.get(i);
-				for (int j = i * BITS_PER_WORD, max = (j + BITS_PER_WORD); j < max
-						&& oldValue != 0; j++) {
+				for (int j = offset + (i * BITS_PER_WORD); oldValue != 0; j++) {
 					if ((oldValue & 1) == 1)
 						result.add(j);
 					oldValue = oldValue >> 1;
@@ -90,16 +90,6 @@ public class AtomicBitSet {
 			}
 		}
 		return result;
-	}
-
-	/**
-	 * Clears all the bits.
-	 * 
-	 */
-	public void clear() {
-		for (AtomicLongArray bitsHolder : bitsHolderMap.values())
-			for (int i = 0; i < bitsHolder.length(); i++)
-				bitsHolder.set(i, 0);
 	}
 
 	/**
@@ -118,4 +108,15 @@ public class AtomicBitSet {
 				return;
 		}
 	}
+
+	/**
+	 * Clears all the bits.
+	 * 
+	 */
+	public void clear() {
+		for (AtomicLongArray bitsHolder : bitsHolderMap.values())
+			for (int i = 0; i < bitsHolder.length(); i++)
+				bitsHolder.set(i, 0);
+	}
+
 }
--- test/org/hashtrees/utils/test/AtomicBitSetTest.java
+++ test/org/hashtrees/utils/test/AtomicBitSetTest.java
@@ -1,10 +1,19 @@
 package org.hashtrees.utils.test;
 
+import java.util.List;
+
 import org.hashtrees.util.AtomicBitSet;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class AtomicBitSetTest {
+
+	@Test
+	public void testGetBit() {
+		AtomicBitSet obj = new AtomicBitSet();
+		List<Integer> setBits = obj.clearAndGetAllSetBits();
+		Assert.assertEquals(0, setBits.size());
+	}
 
 	@Test
 	public void testSetBit() {
@@ -15,8 +24,46 @@ public class AtomicBitSetTest {
 		Assert.assertFalse(obj.get(0));
 
 		obj = new AtomicBitSet();
-		obj.set(1023);
-		Assert.assertTrue(obj.get(1023));
+		obj.set(10000000);
+		Assert.assertTrue(obj.get(10000000));
+		obj.clear(10000000);
+		Assert.assertFalse(obj.get(10000000));
 	}
 
+	@Test
+	public void testSetBits() {
+		AtomicBitSet obj = new AtomicBitSet();
+		obj.set(1025);
+		obj.set(1026);
+		List<Integer> setBits = obj.clearAndGetAllSetBits();
+		Assert.assertEquals(2, setBits.size());
+		Assert.assertTrue(setBits.contains(1025));
+		Assert.assertTrue(setBits.contains(1026));
+	}
+
+	@Test
+	public void testClearBit() {
+		AtomicBitSet obj = new AtomicBitSet();
+		obj.set(1025);
+		obj.set(1026);
+
+		obj.clear(1025);
+		Assert.assertFalse(obj.get(1025));
+		Assert.assertTrue(obj.get(1026));
+
+		List<Integer> setBits = obj.clearAndGetAllSetBits();
+		Assert.assertEquals(1, setBits.size());
+		Assert.assertTrue(setBits.contains(1026));
+	}
+
+	@Test
+	public void testClearBits() {
+		AtomicBitSet obj = new AtomicBitSet();
+		obj.set(1025);
+		obj.set(1026);
+
+		obj.clear();
+		List<Integer> setBits = obj.clearAndGetAllSetBits();
+		Assert.assertEquals(0, setBits.size());
+	}
 }
